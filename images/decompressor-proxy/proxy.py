57c6f92c18802b216080d2235a093b9dedc9cf15
Diego Pereira <rockin.jack@gmail.com>
2020-07-29 02:38:49 -0300

    refactor: code cleanup

--- images/decompressor-proxy/proxy.py
+++ images/decompressor-proxy/proxy.py
@@ -4,7 +4,6 @@ import requests
 base = 'http://tilestache:8000'
 
 def app(environ, start_response):
-    print(environ)
     data = requests.get(f'{base}{environ["PATH_INFO"]}')
     decomp = gzip.decompress(data.content)
     start_response("200 OK", [
