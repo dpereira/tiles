ddb7754c19b779ea980c9a15523c7c5861bb49a7
Diego Pereira <rockin.jack@gmail.com>
2020-07-30 23:04:45 -0300

    feat: add raster layer

--- images/web/tiles.js
+++ images/web/tiles.js
@@ -1,6 +1,7 @@
 import Map from 'ol/Map';
 import View from 'ol/View';
 import VectorTileLayer from 'ol/layer/VectorTile';
+import TileLayer from 'ol/layer/Tile';
 import VectorTileSource from 'ol/source/VectorTile';
 import XYZ from 'ol/source/XYZ';
 import MVT from 'ol/format/MVT';
@@ -8,6 +9,12 @@ import MVT from 'ol/format/MVT';
 new Map({
   target: 'map',
   layers: [
+    new TileLayer({
+      source: new XYZ({
+        url: '/world-raster/{z}/{x}/{y}.png',
+        maxZoom: 8
+      })
+    }),
     new VectorTileLayer({
       source: new VectorTileSource({
         url: '/world/{z}/{x}/{y}.pbf',
